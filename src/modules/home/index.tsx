492017e0346fff2d545e2d1435f1c2f25784ec66
PKhing <apimrak@gmail.com>
2022-05-26 12:40:19 +0700

    ✨ Implement home page

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,13 +1,26 @@
+import Typography from 'common/components/Typography'
 import React from 'react'
 import Card from './components/Card'
+import { CardContainer, Container } from './styled'
 
 const Home = () => {
   return (
-    <div style={{ padding: '15px' }}>
-      <Card time={new Date()} weight={200} />
-      <Card time={new Date()} weight={200} variant="short" />
-      <Card time={new Date()} weight={200} variant="withButton" />
-    </div>
+    <Container>
+      <Typography variant="h1">New Food</Typography>
+      <CardContainer>
+        <Card time={new Date()} weight={200} variant="withButton" />
+        <Card time={new Date()} weight={200} variant="withButton" />
+      </CardContainer>
+
+      <Typography variant="h1" css={{ marginTop: '25px' }}>
+        History
+      </Typography>
+      <CardContainer>
+        <Card time={new Date()} weight={200} variant="short" />
+        <Card time={new Date()} weight={200} variant="short" />
+        <Card time={new Date()} weight={200} variant="short" />
+      </CardContainer>
+    </Container>
   )
 }
 
---
+++ src/modules/home/styled.ts
@@ -0,0 +1,11 @@
+import { styled } from 'config/stitches.config'
+
+export const Container = styled('div', {
+  padding: '15px',
+})
+
+export const CardContainer = styled('div', {
+  display: 'flex',
+  gap: '5px',
+  flexWrap: 'wrap',
+})

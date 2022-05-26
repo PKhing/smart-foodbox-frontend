3a0814283daffdfdb2c054ea948e4bbdd916629a
PKhing <apimrak@gmail.com>
2022-05-26 10:50:09 +0700

    ✨ Add Button component

---
+++ src/common/components/Button/styled.ts
@@ -0,0 +1,32 @@
+import { styled } from 'config/stitches.config'
+
+export const Button = styled('button', {
+  padding: '5px 20px',
+  fontFamily: '$IBMPlexSans',
+  fontWeight: '500',
+  fontSize: '1rem',
+  cursor: 'pointer',
+  border: 'none',
+  borderRadius: '5px',
+  variants: {
+    variant: {
+      primary: {
+        color: 'white',
+        backgroundColor: '$primary500',
+        '&:hover': {
+          backgroundColor: '$primary600',
+        },
+      },
+      secondary: {
+        color: '$primary700',
+        backgroundColor: '$primary200',
+        '&:hover': {
+          backgroundColor: '$primary300',
+        },
+      },
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+  },
+})

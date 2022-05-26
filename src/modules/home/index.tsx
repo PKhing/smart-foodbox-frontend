fa3552f0ae1d1ea803bbe038414a7c29c74693cf
PKhing <apimrak@gmail.com>
2022-05-26 15:04:01 +0700

    💄 Add icon to title

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,5 +1,7 @@
 import Typography from 'common/components/Typography'
 import React from 'react'
+import { MdOutlineHistory } from 'react-icons/md'
+import { HiSparkles } from 'react-icons/hi'
 import { useMediaQuery } from 'react-responsive'
 import Card from './components/Card'
 import { CardContainer, Container } from './styled'
@@ -10,13 +12,19 @@ const Home = () => {
   })
   return (
     <Container>
-      <Typography variant="h1">New Food</Typography>
+      <Typography variant="h1">
+        <HiSparkles style={{ marginBottom: '-4px', marginRight: '5px' }} />
+        New Food
+      </Typography>
       <CardContainer>
         <Card time={new Date()} weight={200} variant="edit" />
         <Card time={new Date()} weight={200} variant="edit" />
       </CardContainer>
 
       <Typography variant="h1" css={{ marginTop: '25px' }}>
+        <MdOutlineHistory
+          style={{ marginBottom: '-4px', marginRight: '5px' }}
+        />
         History
       </Typography>
       <CardContainer>
